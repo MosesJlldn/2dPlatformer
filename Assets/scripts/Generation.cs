691c14a5bbf0fe9f65bb8306bb3da912a1f7e4a2
MosesZ <mozas97@gmail.com>
2018-03-20 00:42:39 +0300

    Initial commit

---
+++ Assets/scripts/Generation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AssemblyCSharp;
+
+public class Generation : MonoBehaviour {
+
+	public Transform v1;
+	public Transform v2;
+	public GameObject parent;
+	public GameObject go;
+	public float roughness;
+	private ArrayList points;
+
+	// Use this for initialization
+	void Start () {
+		
+		points = new ArrayList ();
+		DiamondSquareAlg.calculate (v1.position, v2.position, roughness, points);
+
+		foreach (Vector2 point in points) {
+		
+			Instantiate (go, new Vector3 (point.x, point.y, 0), Quaternion.identity, parent.transform); 
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
