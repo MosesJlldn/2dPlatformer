10f70cfbde055db4c93adacdcc7a33d1b230b074
MosesZ <mozas97@gmail.com>
2018-04-06 13:27:04 +0300

    11
    11

--- Assets/scripts/DiamondSquareAlg.cs
+++ Assets/scripts/DiamondSquareAlg.cs
@@ -12,40 +12,27 @@ namespace AssemblyCSharp
 		{
 		}
 
-		public static void calculate(Vector2 v1, Vector2 v2, float roughness, ArrayList points) {
-
+		public static void calculate(Vector2 v1, Vector2 v2, float roughness, List<Vector2> points) {
+			
 			int length = (int) Math.Round(Vector2.Distance (v1, v2));
 			int horizontal_distance = (int) Math.Round(v2.x - v1.x);
 			int vertical_distance = (int) Math.Round(v2.y - v1.y);
 
-			Debug.Log ("horizontal:" + horizontal_distance);
-			Debug.Log ("vertical:" + vertical_distance);
+			Vector2 result = new Vector2 ((v1.x + v2.x) / 2, (v1.y + v2.y) / 2);
 
+			bool flag = (UnityEngine.Random.value > 0.5f);
+			float displacement = 0;
 
-				
-				Vector2 result = new Vector2 ((v1.x + v2.x) / 2, (v1.y + v2.y) / 2);
+			displacement = flag ? roughness * length : -roughness * length;
 
-				bool flag = (UnityEngine.Random.value > 0.5f);
-				float displacement = 0;
-
-				if (flag)
-					displacement = roughness * length;
-				else
-					displacement = -roughness * length;
-
-				result = new Vector2 (result.x, result.y + displacement);
-				points.Add (result);
+			result = new Vector2 (result.x, result.y + displacement);
+			points.Add (result);
 
 			if (horizontal_distance - 1 > 2) {
 				
 				calculate (v1, result, roughness, points);
 				calculate (result, v2, roughness, points);
-			} 
-//			else {
-				
-//				points.Add (v1 + new Vector2 (1, 0));
-//				points.Add (v1 + new Vector2 (2, 0));
-//			}
+			}
 		}
 	}
 }
